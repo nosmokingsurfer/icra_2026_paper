b4e9d95836776a3771898df862d784ebf0b50599
Gleb <you@example.com>
2025-08-05 15:28:19 +0300

    refactored code before adding tensorboard

--- experiments/ronin_nn_baseline.py
+++ experiments/ronin_nn_baseline.py
@@ -3,36 +3,34 @@ from pathlib import Path
 from torch.utils.data import DataLoader
 import torch
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 sys.path.insert(0,str(Path('./external/ronin/source/').resolve()))
 
 from ronin_resnet import get_dataset_from_list, get_model, run_test
 
-def train_ronin(args):
-    _fc_config = {'fc_dim': 512, 'in_dim': 7, 'dropout': 0.5, 'trans_planes': 128}
-
+def build_datasets(args):
     train_dataset = get_dataset_from_list(args.root_dir, args.train_list, args, mode='train')
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-
-
     val_dataset, val_loader = None, None
     if args.val_list is not None:
         val_dataset = get_dataset_from_list(args.root_dir, args.val_list, args, mode='val')
         val_loader = DataLoader(val_dataset, batch_size=512, shuffle=True)
-
-    device = torch.device('cuda:0' if torch.cuda.is_available() and not args.cpu else 'cpu')
-
-    print(type(device.type), "\n\n\n\n")
-    if device.type == 'cpu':
-        print("_"*50, "\n", "USING CPU FOR TRAININ", "_"*50, "\n")
-
-    #TODO: add checkpoints< logs and etc placement 
-
-    _fc_config['in_dim'] = args.window_size // 32 + 1
-
-    network = get_model(args.arch).to(device)
     print('Number of train samples: {}'.format(len(train_dataset)))
     if val_dataset:
         print('Number of val samples: {}'.format(len(val_dataset)))
+
+    return train_dataset, train_loader, val_dataset, val_loader
+
+
+def train_ronin(args, fc_config):
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() and not args.cpu else 'cpu')
+    if device.type == 'cpu':
+        print("_"*50, "\n", "USING CPU FOR TRAININ", "_"*50, "\n")
+
+    train_dataset, train_loader, val_dataset, val_loader = build_datasets(args)
+
+    network = get_model(args.arch).to(device)
     total_params = network.get_num_params()
     print('Total number of parameters: ', total_params)
 
@@ -40,37 +38,31 @@ def train_ronin(args):
     optimizer = torch.optim.Adam(network.parameters(), args.lr)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=10, eps=1e-12)
     train_losses_all, val_losses_all = [], []
-    start_epoch = 0
-    # if args.continue_from is not None and osp.exists(args.continue_from):
-    #     checkpoints = torch.load(args.continue_from)
-    #     start_epoch = checkpoints.get('epoch', 0)
-    #     network.load_state_dict(checkpoints.get('model_state_dict'))
-    #     optimizer.load_state_dict(checkpoints.get('optimizer_state_dict'))
 
     init_train_targ, init_train_pred = run_test(network, train_loader, device, eval_mode=False)
 
     init_train_loss = np.mean((init_train_targ - init_train_pred) ** 2, axis=0)
     train_losses_all.append(np.mean(init_train_loss))
     best_val_loss = np.inf
+
     try:
-        for epoch in range(start_epoch, args.epochs):
+        for epoch in range(args.epochs):
             network.train()
             train_outs, train_targets = [], []
             for batch_id, (feat, targ, _, _) in enumerate(train_loader):  # feat targ and ???????? why do we even need to more args if we do not us it?
                 feat, targ = feat.to(device), targ.to(device)
                 optimizer.zero_grad()
-
                 pred = network(feat)
                 train_outs.append(pred.cpu().detach().numpy())
                 train_targets.append(targ.cpu().detach().numpy())
-                loss = criterion(pred, targ)
-                loss = torch.mean(loss)
-                loss.backward()
+                train_loss = criterion(pred, targ)
+                train_loss = torch.mean(train_loss)
+                train_loss.backward()
                 optimizer.step()
             train_outs = np.concatenate(train_outs, axis=0)
             train_targets = np.concatenate(train_targets, axis=0)
             train_losses = np.average((train_outs - train_targets) ** 2, axis=0)
-            print(111111111111111111111111)
+
             if val_loader is not None:
                 network.eval()
                 val_outs, val_targets = run_test(network, val_loader, device)
@@ -82,13 +74,6 @@ def train_ronin(args):
                 val_losses_all.append(avg_loss)
                 if avg_loss < best_val_loss:
                     best_val_loss = avg_loss
-                    # TODO: add model saving
-                    # if args.out_dir and osp.isdir(args.out_dir):
-                    #     model_path = osp.join(args.out_dir, 'checkpoints', 'checkpoint_%d.pt' % epoch)
-                    #     torch.save({'model_state_dict': network.state_dict(),
-                    #                 'epoch': epoch,
-                    #                 'optimizer_state_dict': optimizer.state_dict()}, model_path)
-                    #     print('Model saved to ', model_path)
 
     except KeyboardInterrupt:
         print('-' * 60)
@@ -128,6 +113,8 @@ if __name__ == '__main__':
     parser.add_argument('--target_sigma', type=float, default=0.00001)
 
     args = parser.parse_args()
+    fc_config = {'fc_dim': 512, 'in_dim': 7, 'dropout': 0.5, 'trans_planes': 128}
+    fc_config['in_dim'] = args.window_size // 32 + 1
     print(type(args))
 
-    train_ronin(args)
+    train_ronin(args, fc_config)
